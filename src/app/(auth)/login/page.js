3b7c3c04f44c3cbc7c25fa2d9c89b5eae2933024
NguyenXuanVinh <nongami87@gmail.com>
2025-08-03 08:49:45 +0700

    refactor: remove social sign-in and help options from login

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,7 +1,5 @@
 import LoginForm from "./_components/login-form";
-import SignInWith from "./_components/sign-in-with";
 import LoginLeading from "./_components/login-leading";
-import LoginGetHelp from "./_components/login-get-help";
 import LoginTrailing from "./_components/login-trailing";
 
 function Login() {
@@ -9,8 +7,6 @@ function Login() {
     <div className="w-full max-w-md">
       <LoginLeading />
       <LoginForm />
-      <SignInWith />
-      <LoginGetHelp />
       <LoginTrailing />
     </div>
   );
