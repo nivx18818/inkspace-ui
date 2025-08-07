e04b3fcf50ff09e0306b3ce8a92fa183bfcb338e
NguyenXuanVinh <nongami87@gmail.com>
2025-08-07 16:17:53 +0700

    refactor: replace auth state from isAuthenticated to success

--- src/store/hooks/use-auth.js
+++ src/store/hooks/use-auth.js
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 
 const useAuth = () => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const success = useSelector((state) => state.auth.success);
   const isLoading = useSelector((state) => state.auth.isLoading);
   const error = useSelector((state) => state.auth.error);
   const checkResult = useSelector((state) => state.auth.checkResult);
-  return { isAuthenticated, isLoading, error, checkResult };
+  return { success, isLoading, error, checkResult };
 };
 
 export default useAuth;
