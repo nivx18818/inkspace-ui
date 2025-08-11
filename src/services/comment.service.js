4d726bfab6e2a07d00a4b740502c55b312ddc8ad
NguyenXuanVinh <nongami87@gmail.com>
2025-08-11 17:27:52 +0700

    feat: enable credentials for comment creation

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,6 +1,8 @@
 import httpRequest from "@/utils/http-request";
 
 export const create = async (postSlug, data) => {
-  const res = await httpRequest.post(`/posts/${postSlug}/comments`, data);
+  const res = await httpRequest.post(`/posts/${postSlug}/comments`, data, {
+    withCredentials: true,
+  });
   return res;
 };
