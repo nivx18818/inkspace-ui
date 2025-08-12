e740ba29d93f1f6f5e1b75278e0a65f03b9e2e80
NguyenXuanVinh <nongami87@gmail.com>
2025-08-12 20:12:27 +0700

    feat: redirect authenticated users to home

--- src/components/user-fetching/index.jsx
+++ src/components/user-fetching/index.jsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { authThunks } from "@/store/thunks";
 import useAuth from "@/store/hooks/use-auth";
 
 function UserFetching() {
+  const router = useRouter();
   const dispatch = useDispatch();
   const { success } = useAuth();
 
   useEffect(() => {
     dispatch(authThunks.me());
+    if (success) router.push("/");
   }, [dispatch, success]);
 
   return null;
